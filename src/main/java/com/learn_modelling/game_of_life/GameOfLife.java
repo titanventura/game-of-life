e144b0c683230f3009060a4da25e5a4e0b01240e
aswath.s <aswath.s@thoughtworks.com>
2022-04-15 12:35:13 +0530

    Refactor. Move forloops in generation iteration to lambdas. Extract tick steps as methods

--- src/main/java/com/learn_modelling/game_of_life/GameOfLife.java
+++ src/main/java/com/learn_modelling/game_of_life/GameOfLife.java
@@ -12,9 +12,9 @@ public class GameOfLife {
     private HashSet<Cell> visited;
 
 
-
     public GameOfLife() {
         this.cells = new ArrayList<>();
+        resetTick();
     }
 
     public void seed(List<Cell> seedCells) {
@@ -26,38 +26,44 @@ public class GameOfLife {
     }
 
     public void tick() {
+        initTick();
+        updateGeneration();
+        resetTick();
+    }
+
+    private void initTick() {
         nextGeneration = new ArrayList<>();
         visited = new HashSet<>();
+    }
 
-        for (Cell cell : cells) {
-
+    private void updateGeneration() {
+        this.cells.forEach(cell -> {
             updateCell(cell);
             List<Cell> allNeighbours = cell.allNeighbours();
+            allNeighbours.forEach(this::updateCell);
+        });
+        this.cells = nextGeneration;
+    }
 
-            for (Cell neighbour : allNeighbours) {
-                updateCell(neighbour);
-            }
-        }
-
-        cells = nextGeneration;
-
-        nextGeneration = null;
-        visited = null;
+    private void resetTick() {
+        this.nextGeneration = null;
+        this.visited = null;
     }
 
     private void updateCell(Cell cell) {
-        if (visited.contains(cell)) return;
+        if (this.visited.contains(cell)) return;
 
-        visited.add(cell);
+        this.visited.add(cell);
 
         List<Cell> allNeighbours = cell.allNeighbours();
         int noOfLivingNeighbours = filterLivingCells(allNeighbours).size();
 
-        if (hasLivingCell(cell)) {
-            if (noOfLivingNeighbours == 2 || noOfLivingNeighbours == 3) nextGeneration.add(cell);
-        } else {
-            if (noOfLivingNeighbours == 3) nextGeneration.add(cell);
+        if (!hasLivingCell(cell) && noOfLivingNeighbours == 3) {
+            this.nextGeneration.add(cell);
+            return;
         }
+
+        if (noOfLivingNeighbours == 2 || noOfLivingNeighbours == 3) this.nextGeneration.add(cell);
     }
 
     private List<Cell> filterLivingCells(List<Cell> cells) {
